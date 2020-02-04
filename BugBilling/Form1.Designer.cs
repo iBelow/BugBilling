647fad8bc6b68c1f1449253129bad83f4cd25d06
Артур Бойко <a.boyko@PATIO-PLUS.LOC>
2020-02-04 14:41:09 +0300

    Добавьте файлы проекта.

---
+++ BugBilling/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace BugBilling {
+    partial class MainUI {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainUI));
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tstGamepathlabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ScreenBox = new System.Windows.Forms.CheckBox();
+            this.donebox = new System.Windows.Forms.CheckBox();
+            this.zipBox = new System.Windows.Forms.CheckBox();
+            this.LauncherLogsCheckbox = new System.Windows.Forms.CheckBox();
+            this.GameLogCheckbox = new System.Windows.Forms.CheckBox();
+            this.gamePathCheckBox = new System.Windows.Forms.CheckBox();
+            this.dxdiagchkbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Make report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tstGamepathlabel,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 156);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(554, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(63, 22);
+            this.toolStripLabel1.Text = "Game Path:";
+            this.toolStripLabel1.ToolTipText = "Путь к установленной игре";
+            // 
+            // tstGamepathlabel
+            // 
+            this.tstGamepathlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tstGamepathlabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.tstGamepathlabel.Name = "tstGamepathlabel";
+            this.tstGamepathlabel.Size = new System.Drawing.Size(53, 22);
+            this.tstGamepathlabel.Text = "Unknown";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.BackgroundImage = global::BugBilling.Properties.Resources._512px_Telegram_logo_svg;
+            this.toolStripButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ScreenBox);
+            this.groupBox1.Controls.Add(this.donebox);
+            this.groupBox1.Controls.Add(this.zipBox);
+            this.groupBox1.Controls.Add(this.LauncherLogsCheckbox);
+            this.groupBox1.Controls.Add(this.GameLogCheckbox);
+            this.groupBox1.Controls.Add(this.gamePathCheckBox);
+            this.groupBox1.Controls.Add(this.dxdiagchkbox);
+            this.groupBox1.Location = new System.Drawing.Point(419, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 145);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // ScreenBox
+            // 
+            this.ScreenBox.AutoSize = true;
+            this.ScreenBox.Enabled = false;
+            this.ScreenBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ScreenBox.Location = new System.Drawing.Point(6, 101);
+            this.ScreenBox.Name = "ScreenBox";
+            this.ScreenBox.Size = new System.Drawing.Size(62, 17);
+            this.ScreenBox.TabIndex = 6;
+            this.ScreenBox.Text = "Screens";
+            this.ScreenBox.UseVisualStyleBackColor = true;
+            this.ScreenBox.Visible = false;
+            // 
+            // donebox
+            // 
+            this.donebox.AutoSize = true;
+            this.donebox.Enabled = false;
+            this.donebox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.donebox.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.donebox.Location = new System.Drawing.Point(6, 124);
+            this.donebox.Name = "donebox";
+            this.donebox.Size = new System.Drawing.Size(49, 17);
+            this.donebox.TabIndex = 5;
+            this.donebox.Text = "Done";
+            this.donebox.UseVisualStyleBackColor = true;
+            // 
+            // zipBox
+            // 
+            this.zipBox.AutoSize = true;
+            this.zipBox.Enabled = false;
+            this.zipBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.zipBox.Location = new System.Drawing.Point(6, 85);
+            this.zipBox.Name = "zipBox";
+            this.zipBox.Size = new System.Drawing.Size(79, 17);
+            this.zipBox.TabIndex = 4;
+            this.zipBox.Text = "ZIP Archive";
+            this.zipBox.UseVisualStyleBackColor = true;
+            // 
+            // LauncherLogsCheckbox
+            // 
+            this.LauncherLogsCheckbox.AutoSize = true;
+            this.LauncherLogsCheckbox.Enabled = false;
+            this.LauncherLogsCheckbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LauncherLogsCheckbox.Location = new System.Drawing.Point(6, 69);
+            this.LauncherLogsCheckbox.Name = "LauncherLogsCheckbox";
+            this.LauncherLogsCheckbox.Size = new System.Drawing.Size(89, 17);
+            this.LauncherLogsCheckbox.TabIndex = 3;
+            this.LauncherLogsCheckbox.Text = "Launcher Log";
+            this.LauncherLogsCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // GameLogCheckbox
+            // 
+            this.GameLogCheckbox.AutoSize = true;
+            this.GameLogCheckbox.Enabled = false;
+            this.GameLogCheckbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GameLogCheckbox.Location = new System.Drawing.Point(6, 53);
+            this.GameLogCheckbox.Name = "GameLogCheckbox";
+            this.GameLogCheckbox.Size = new System.Drawing.Size(72, 17);
+            this.GameLogCheckbox.TabIndex = 2;
+            this.GameLogCheckbox.Text = "Game Log";
+            this.GameLogCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // gamePathCheckBox
+            // 
+            this.gamePathCheckBox.AutoSize = true;
+            this.gamePathCheckBox.Enabled = false;
+            this.gamePathCheckBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gamePathCheckBox.Location = new System.Drawing.Point(6, 19);
+            this.gamePathCheckBox.Name = "gamePathCheckBox";
+            this.gamePathCheckBox.Size = new System.Drawing.Size(76, 17);
+            this.gamePathCheckBox.TabIndex = 1;
+            this.gamePathCheckBox.Text = "Game Path";
+            this.gamePathCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // dxdiagchkbox
+            // 
+            this.dxdiagchkbox.AutoSize = true;
+            this.dxdiagchkbox.Enabled = false;
+            this.dxdiagchkbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dxdiagchkbox.Location = new System.Drawing.Point(6, 36);
+            this.dxdiagchkbox.Name = "dxdiagchkbox";
+            this.dxdiagchkbox.Size = new System.Drawing.Size(64, 17);
+            this.dxdiagchkbox.TabIndex = 0;
+            this.dxdiagchkbox.Text = "DXDIAG";
+            this.dxdiagchkbox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Version:";
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Location = new System.Drawing.Point(48, 134);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(29, 13);
+            this.versionLabel.TabIndex = 8;
+            this.versionLabel.Text = "NaN";
+            // 
+            // MainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::BugBilling.Properties.Resources.War_Thunder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(554, 181);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(570, 220);
+            this.MinimumSize = new System.Drawing.Size(570, 220);
+            this.Name = "MainUI";
+            this.Text = "War Thunder report tool";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel tstGamepathlabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox dxdiagchkbox;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.CheckBox LauncherLogsCheckbox;
+        private System.Windows.Forms.CheckBox GameLogCheckbox;
+        private System.Windows.Forms.CheckBox gamePathCheckBox;
+        private System.Windows.Forms.CheckBox donebox;
+        private System.Windows.Forms.CheckBox zipBox;
+        private System.Windows.Forms.CheckBox ScreenBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label versionLabel;
+    }
+}
+
---
+++ BugBilling/Form1.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+
+namespace BugBilling {
+    public partial class MainUI : Form {
+
+        private string WorkDirectory = string.Empty; //for export logs
+        private string GameDir = string.Empty; //Get current game path via regedit and save here :3
+        private string GameLogPath = string.Empty; //GameLog last file path
+        private string launcher_logPath = string.Empty; //launcherlog
+        private string Screenpath = string.Empty; //
+
+        public MainUI() {
+            InitializeComponent();
+            GetGF();
+        }
+
+        /// <summary>
+        /// Get latest logs from folders
+        /// </summary>
+        private void GetLogs() {
+            try {
+                //.clog
+                FileInfo gLogFile = Directory.EnumerateFiles(Path.Combine(GameDir, ".game_logs"), "*.clog")
+                .Select(file_path => new FileInfo(file_path))
+                .OrderByDescending(file_info => file_info.LastWriteTime)
+                .FirstOrDefault();
+
+                //launcher log (.txt)
+                FileInfo launcherLogFile = Directory.EnumerateFiles(Path.Combine(GameDir, ".launcher_log"), "*.txt")
+               .Select(file_path => new FileInfo(file_path))
+               .OrderByDescending(file_info => file_info.LastWriteTime)
+               .FirstOrDefault();
+
+                if (gLogFile == null) {
+                    // No logs
+                    MessageBox.Show("No logs in .game_logs directory", "Warning");
+                }
+                if (launcherLogFile == null) {
+                    MessageBox.Show("No logs in .launcher_log directory", "Warning");
+
+                } else {
+                    // Find last file
+                    GameLogPath = gLogFile.FullName.ToString();
+                    launcher_logPath = launcherLogFile.FullName.ToString();
+                    gLogFile.CopyTo(Path.Combine(WorkDirectory, gLogFile.Name), true);
+                    GameLogCheckbox.CheckState = CheckState.Checked;
+                    launcherLogFile.CopyTo(Path.Combine(WorkDirectory, launcherLogFile.Name), true);
+                    LauncherLogsCheckbox.CheckState = CheckState.Checked;
+
+                }
+            } catch (Exception ex) {
+                MessageBox.Show($"Can't find logs.\n{ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Packing archive 
+        /// </summary>
+        private void Archiving() {
+            using (ZipFile zip = new ZipFile()) {
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestSpeed;
+                ZipEntry ze = zip.AddDirectory(WorkDirectory);
+                FolderBrowserDialog fdb = new FolderBrowserDialog();
+                zip.Save($"{WorkDirectory}.zip");
+                zipBox.CheckState = CheckState.Checked;
+            }
+            if (dxdiagchkbox.Checked && GameLogCheckbox.Checked && LauncherLogsCheckbox.Checked && zipBox.Checked) {
+                donebox.CheckState = CheckState.Checked;
+                FileInfo outputFile = new FileInfo(Path.Combine(WorkDirectory, WorkDirectory + ".zip"));
+                MessageBox.Show($@"Done, archive saved as {WorkDirectory}.zip", "Done");
+                ExploreFile($"{WorkDirectory}.zip");
+            }
+            
+        }
+        /// <summary>
+        /// No comment xD
+        /// </summary>
+        private void GetGF() {
+            GameDir = GetGamePath();
+            try {
+                versionLabel.Text = Application.ProductVersion.ToString();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        /// <summary>
+        /// clear checkboxes
+        /// </summary>
+        private void ClearBoxes() {
+            dxdiagchkbox.CheckState = CheckState.Unchecked;
+            GameLogCheckbox.CheckState = CheckState.Unchecked;
+            LauncherLogsCheckbox.CheckState = CheckState.Unchecked;
+            zipBox.CheckState = CheckState.Unchecked;
+            donebox.CheckState = CheckState.Unchecked;
+            ScreenBox.CheckState = CheckState.Unchecked;
+            ScreenBox.Visible = false;
+        }
+
+        //Start here
+
+        private void button1_Click(object sender, EventArgs e) {
+            button1.Enabled = false;
+            ClearBoxes();
+            WorkDirectory = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), $@"WTRT {Guid.NewGuid()}")).ToString();
+            DialogResult result = MessageBox.Show("Do you want add screens?", "WT screens", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes) {
+                ScreenBox.Visible = true;
+                using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                    openFileDialog.InitialDirectory = Path.Combine(GameDir, "Screenshots");
+                    openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|jpeg files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 3;
+                    openFileDialog.Multiselect = true;
+                    openFileDialog.RestoreDirectory = true;
+                    if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                        foreach (String screens in openFileDialog.FileNames) {
+                            try {
+                                Screenpath = screens;
+                                FileInfo file = new FileInfo(Screenpath);
+                                file.CopyTo($@"{WorkDirectory}\\{file.Name}", true);
+                                ScreenBox.CheckState = CheckState.Checked;
+                            } catch (Exception ex) {
+                                MessageBox.Show(ex.Message);
+                            }
+                        }
+                    }
+                }
+            }
+            try {
+                RunDxDiag();
+                GetLogs();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+           
+
+
+        /// <summary>
+        /// Setup Game folder
+        /// </summary>
+        /// <returns></returns>
+        private string GetGamePath() {
+            try {
+                RegistryKey currentKey = Registry.CurrentUser;
+                RegistryKey warhunderRegeditPath = currentKey.OpenSubKey(@"Software\\Gaijin\\WarThunder");
+                string GamePath = warhunderRegeditPath.GetValue("Path").ToString().Replace("launcher.exe", null);
+                currentKey.Close();
+                tstGamepathlabel.Text = GamePath;
+                gamePathCheckBox.CheckState = CheckState.Checked;
+                return GamePath;
+            } catch (Exception ex) {
+                MessageBox.Show("Can't find WarThunder folder" + ex.Message);
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                folderBrowserDialog.ShowNewFolderButton = false;
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK) {
+                    GameDir = folderBrowserDialog.SelectedPath;
+                    tstGamepathlabel.Text = GameDir;
+                    gamePathCheckBox.CheckState = CheckState.Checked;
+                    GameLogCheckbox.CheckState = CheckState.Checked;
+                    LauncherLogsCheckbox.CheckState = CheckState.Checked;
+                }
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Get dxdiag for any platform. Saving in Current running directory
+        /// </summary>
+        private async void RunDxDiag() {
+            var psi = new ProcessStartInfo();
+            if (IntPtr.Size == 4 && Environment.Is64BitOperatingSystem) {
+                // 64-bit
+                psi.FileName = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.Windows),
+                    "sysnative\\dxdiag.exe");
+            } else {
+                psi.FileName = Path.Combine(
+                    Environment.SystemDirectory,
+                    "dxdiag.exe");
+            }
+            string path = Path.GetTempFileName();
+            try {
+                psi.Arguments = "/t " + path;
+                using (var prc = Process.Start(psi)) {
+                    await Task.Run(() => prc.WaitForExit());
+                    if (prc.ExitCode != 0) {
+                        throw new Exception("DXDIAG failed with exit code " + prc.ExitCode.ToString());
+                    }
+                }
+                //ToDo push data to msgbox
+                string res = File.ReadAllText(path);
+            } finally {
+                try {
+                    File.Copy(path, WorkDirectory + @"\dxdiag.log", true);
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+                dxdiagchkbox.CheckState = CheckState.Checked;
+                Archiving();
+            }
+        }
+        public bool ExploreFile(string filePath) {
+            if (!File.Exists(filePath)) {
+                return false;
+            }
+            //Clean up file path so it can be navigated OK
+            filePath = Path.GetFullPath(filePath);
+            Process.Start("explorer.exe", string.Format("/select,\"{0}\"", filePath));
+            button1.Enabled = true;
+            return true;
+            
+        }
+
+        /// <summary>
+        /// telegram group link
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton1_Click(object sender, EventArgs e) {
+            Process.Start("https://t.me/WarThunderBlog");
+        }
+    }
+
+
+}
+
